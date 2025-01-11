bb22523bdad6219863aa6eb480336e5ceebaa7ea
DongGunYoon <donggun961108@gmail.com>
2025-01-12 08:56:55 +0900

    donggun - [PCCE 기출문제] 7번 / 버스 외 3

---
+++ season1.5/week01/[02] 문자 출력/donggun/문자 출력.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string message = "Let's go!";
+
+    cout << "3\n2\n1" << endl;
+    cout << message << endl;
+
+    return 0;
+}
